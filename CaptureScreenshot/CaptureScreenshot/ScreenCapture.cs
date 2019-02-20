f0ed7e9fe60fdcb14607be5e301fc15dbe7c2f86
UncleAlexKun <raisin27@mail.ru>
2019-02-20 22:20:28 +0400

    Настройка Nlog
    Конфигурировал NLog.config для логирования ошибок

--- CaptureScreenshot/CaptureScreenshot/ScreenCapture.cs
+++ CaptureScreenshot/CaptureScreenshot/ScreenCapture.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using CaptureScreenshot.WinAPI;
 
 namespace CaptureScreenshot
 {
 	public class ScreenCapture
 	{
-
+		public Image CaptureScreen()
+		{
+			return CaptureWindow(User32.GetDesktopWindow());
+		}
 	}
 }
